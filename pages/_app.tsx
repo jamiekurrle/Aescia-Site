1d950cbe9e4bbe7fa4a4901437a72ae01a9724aa
jamiekurrle <jamie.kurrle@gmail.com>
2026-01-18 01:05:11 -0500

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import { useRouter } from "next/router";
+import "../styles/globals.css";
 
 const SITE_URL = "https://aesciahealth.com";
 
